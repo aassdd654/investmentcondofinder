88b78fb06883e397442956ac5c9a4c79e163ce97
Li <chuli@iMac-Li.local>
2017-08-17 12:02:12 -0700

    edit

--- hello.py
+++ hello.py
@@ -2,9 +2,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/")
 def index():
-    return '<h1>Hello Test Flask Yo</h1>'
+    return '<h1>index page</h1>'
+
+@app.route("/hello")
+def hello():
+    return '<h1>hello page: Test Flask Yo</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
