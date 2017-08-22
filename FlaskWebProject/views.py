e918e4f97979415bfe540d1fc491ffe62eec9ecb
Li <chuli@iMac-Li.local>
2017-08-21 23:20:35 -0700

    initial commit

---
+++ FlaskWebProject/views.py
@@ -0,0 +1,29 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from FlaskWebProject import app
+
+@app.route('/')
+def hello():
+    return "Index Page"
+    
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+
+@app.route('/awesome')
+def awesome():
+    """Renders awesome"""
+    return render_template(
+        'awesome.html',
+        title='AWESOME',
+    )
