85a4664399f4dc1b5500c1d4f4a9502a2d16afb4
Li <chuli@iMac-Li.local>
2017-08-27 04:11:15 -0700

    form

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -3,7 +3,14 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'investmentcondofinder'
+bootstrap=Bootstrap(app)
+moment = Moment(app)
 
 #import FlaskWebProject.views
 import FlaskWebProject.investmentcondofinder
--- FlaskWebProject/investmentcondofinder.py
+++ FlaskWebProject/investmentcondofinder.py
@@ -1,37 +1,59 @@
 
 # coding: utf-8
 from datetime import datetime
-from flask import render_template
+from flask import Flask, render_template, session, redirect, url_for
 from FlaskWebProject import app
 
+# ...............................................#
+# form #
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
-@app.route('/')
-def hello():
-    return "Index Page"
+# ...............................................#
 
-@app.route('/home')
+class NameForm(FlaskForm):
+    name = StringField('Choose The Zipcode:', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+# ...............................................#
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm() #this line passes the class NameForm(FlaskForm)'s attributes to new form
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('investmentcondofinder.html',form=form,
+    name=session.get('name'),title='Investment Condo Finder',
+    current_time=datetime.utcnow())
+
+
+# ...............................................#
+
+@app.route('/contact')
 def home():
-    """Renders the home page."""
+    """Renders feedback page."""
     return render_template(
-        'index.html',
-        title='Home Page',
+        'contact.html',
+        title='contact',
         year=datetime.now().year,
     )
 
-
-@app.route('/awesome')
+@app.route('/about')
 def awesome():
-    """Renders awesome"""
+    """Renders About Us Introduction of the project and me"""
     return render_template(
-        'awesome.html',
-        title='AWESOME',
+        'about.html',
+        title='about us',
     )
 
-# In[6]:
-@app.route('/investmentcondofinder')
-def investmentcondofinder():
-    """Renders investmentcondofinder"""
-    return render_template(
-        'investmentcondofinder.html',
-        title='investmentcondofinder',
-    )
+# ...............................................#
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
